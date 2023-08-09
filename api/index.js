ffe597f6ce51ecf17e6392ff233d79a7e7173223
uves-shaikh <suves58@gmail.com>
2023-08-09 15:40:32 +0530

    commit
    coomit

--- api/index.js
+++ api/index.js
@@ -2,12 +2,16 @@ const express = require('express')
 const cors = require('cors')
 const { default: mongoose } = require('mongoose')
 const User = require('./models/user')
+const Post = require('./models/post')
 const app = express()
 const bcrypt = require('bcrypt');
 const salt = bcrypt.genSaltSync(10);
 const jwt = require('jsonwebtoken')
 const secret = 'dhhiy4hdjk3jhkjd989hjkds'
 const cookieParser = require('cookie-parser')
+const multer = require('multer')
+const uploadMiddleware = multer({ dest: 'uploads/ ' })
+const fs = require('fs')
 
 // const corsOptions = {
 //     origin: 'http://localhost:3000', // Your frontend's URL
@@ -17,7 +21,7 @@ const cookieParser = require('cookie-parser')
 // app.use(cors(corsOptions));
 
 
-app.use(cors({credentials:true,origin:'http://localhost:3000'}))
+app.use(cors({ credentials: true, origin: 'http://localhost:3000' }))
 app.use(express.json())
 app.use(cookieParser())
 
@@ -43,10 +47,10 @@ app.post('/login', async (req, res) => {
     // res.json(passOk)
     if (passOk) {
         // User is logged in
-        jwt.sign({username,id:userDoc._id }, secret, {}, (err, token) => {
+        jwt.sign({ username, id: userDoc._id }, secret, {}, (err, token) => {
             if (err) throw err;
-            res.cookie('token',token).json({
-                id:userDoc._id,
+            res.cookie('token', token).json({
+                id: userDoc._id,
                 username,
             })
         })
@@ -56,16 +60,34 @@ app.post('/login', async (req, res) => {
 })
 // mongodb+srv://lifecanvas:lifecanvas@cluster0.ezefyqm.mongodb.net/?retryWrites=true&w=majority
 
-app.get('/profile', (req,res) => {
-    const {token} = req.cookies;
-    jwt.verify(token, secret, {}, (err,info) => {
-        if(err) throw err
+app.get('/profile', (req, res) => {
+    const { token } = req.cookies;
+    jwt.verify(token, secret, {}, (err, info) => {
+        if (err) throw err
         res.json(info)
     })
 })
 
-app.post('/logout', (req,res) => {
-    res.cookie('token','').json('ok')
+app.post('/logout', (req, res) => {
+    res.cookie('token', '').json('ok')
+})
+
+app.post('/post', uploadMiddleware.single('file'), async (req, res) => {
+    const { originalname, path } = req.file;
+    const parts = originalname.split('.')
+    const ext = parts[parts.length - 1]
+    const newPath = path+'.'+ ext
+    fs.renameSync(path, newPath)
+
+    const { title, summary, content } = req.body;
+    const postDoc = await Post.create({
+        title,
+        summary,
+        content,
+        cover:newPath,
+    })
+
+    res.json(postDoc)
 })
 
 app.listen(4000)
